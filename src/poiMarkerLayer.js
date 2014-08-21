b0ddad5c33d38b89ca3f3f02f61bd5fc728460a6
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-08-21 05:59:20 +0530

    memory cache test

--- src/poiMarkerLayer.js
+++ src/poiMarkerLayer.js
@@ -25,6 +25,8 @@ const Champlain = imports.gi.Champlain;
 const POI = imports.poi;
 const Overpass = imports.overpass;
 const POIMarker = imports.poiMarker;
+const GeoMath = imports.geoMath;
+const MemoryCache = imports.memoryCache;
 
 const POIMarkerLayer = new Lang.Class({
     Name: 'POIMarkerLayer',
@@ -37,6 +39,7 @@ const POIMarkerLayer = new Lang.Class({
         this.parent(params);
 
         this._initOverpass();
+        this._cache = new MemoryCache.MemoryCache({});
 
         let view = this._mapView.view;
         view.connect('notify::latitude', this._onViewMoved.bind(this));
@@ -55,6 +58,63 @@ const POIMarkerLayer = new Lang.Class({
         }
     },
 
+    getTilesInBBOX: function(bbox) {
+
+        let zoom = this._mapView.view.zoom_level;
+        let minX = GeoMath.longitudeToTile(bbox.left, zoom);
+        let minY = GeoMath.latitudeToTile(bbox.top, zoom);
+        let maxX = GeoMath.longitudeToTile(bbox.right, zoom);
+        let maxY = GeoMath.latitudeToTile(bbox.bottom, zoom);
+
+        let tiles = [];
+        for (let x = minX; x <= maxX; x++) {
+            for (let y = minY; y <= minY; y++) {
+                tiles.push(new Champlain.Tile({ x: x,
+                                                y: y,
+                                                zoom_level: zoom,
+                                                size: 256 }));
+            }
+        }
+
+        return tiles;
+    },
+
+    clusterPOIsInTiles: function(tiles, pois) {
+
+        let bboxes = tiles.map(GeoMath.bboxFromTile);
+        let tilesContent = [];
+        for (let i = 0; i < bboxes.length; i++) {
+            tilesContent.push([]);
+            bboxes[i].left = bboxes[i].left.toFixed(7);
+            bboxes[i].right = bboxes[i].right.toFixed(7);
+            bboxes[i].top = bboxes[i].top.toFixed(7);
+            bboxes[i].bottom = bboxes[i].bottom.toFixed(7);
+        }
+        let x = 0;
+        pois.forEach((function(poi) {
+            for (let i = 0; i < bboxes.length; i++) {
+                log('--------------------------');
+                log(poi.lat + ' , ' + poi.lon);
+                log(bboxes[i].left + ' , ' + bboxes[i].right);
+                log(bboxes[i].top + ' , ' + bboxes[i].bottom);
+                log('--------------------------');
+                if (bboxes[i].covers(poi.lat, poi.lon)) {
+                    tilesContent[i].push(poi);
+                    log(x);
+                    break;
+                }
+            }
+            x++;
+        }).bind(this));
+
+        let s = 0;
+        for (let i = 0; i < tilesContent.length; i++) {
+            s += tilesContent[i].length;
+        }
+
+        log(s + ' , ' + pois.length);
+    },
+
     _onViewMoved: function() {
 
         if (this._mapView.view.zoom_level < POI.MIN_DISPLAY_ZOOM_LEVEL)
@@ -63,12 +123,16 @@ const POIMarkerLayer = new Lang.Class({
         this.remove_all();
         let bbox = this._mapView.view.get_bounding_box();
         this._overpassQuery.send(bbox, (function(pois) {
+
             let places = pois.map(Overpass.convertJSONPlaceToPOI);
+            let tiles = this.getTilesInBBOX(this._mapView.view.get_bounding_box());
+            this.clusterPOIsInTiles(tiles, pois);
             places.forEach((function(place) {
                 let poiMarker = new POIMarker.POIMarker({ place: place,
                                                           mapView: this._mapView });
                 poiMarker.addToLayer(this);
             }).bind(this));
+
         }).bind(this));
     }
 });
